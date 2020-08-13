8d9646365193311ca45c52b0f72a06072764dbd0
James Chen <69646422+Jamschem@users.noreply.github.com>
2020-08-13 11:00:47 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is James");
+		System.out.println("I like playing videogames");
+		System.out.println("I don't have a preference for college");
+		System.out.println("I would like to major in CompSci");
+		System.out.println("I want to learn more about coding");
+	}
+};
